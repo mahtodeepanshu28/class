269bc21cc0928e45b69534bfe8ff19cdbe2810ee
Deepanshu <90448300+mahtodeepanshu28@users.noreply.github.com>
2022-03-04 09:15:00 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,115 @@
+// let a=10
+// if(a==10){
+
+// } else{
+
+// }
+
+// //more than one condition:
+// let a=-10
+// if(a<0){
+//     console.log("negative");
+
+// }else if(a==0){
+//     console.log("Zero");
+// }
+// else{
+//     console.log("positive");
+// }
+// switch-case:
+// let gender="m"
+// switch(gender)
+// {
+//     case "m":
+//         console.log("Male");
+//         break;
+//         case "f":
+//             console.log("Female");
+//             break;
+//             default:
+//             console.log("TG");
+//             break;
+// }
+
+
+// let age=13
+
+// switch(true){
+//     case age<18:
+//         console.log("child");
+//         break;
+//     case 18 < age <40:
+//         console.log("adult");
+//         break;
+//      default:
+//         console.log("old age");
+//         break;
+        
+// }
+
+// var dayname = 3
+// switch(dayname){
+//     case 1 :
+//         console.log("monday");
+//         break;
+//     case 2 :
+//         console.log("tuesday");
+//         break;
+//     case 3 :
+//         console.log("wednesday");
+//         break;
+//     case 4 :
+//         console.log("thursday");
+//         break;
+//     case 5 :
+//         console.log("Saturday");
+//         break;
+    
+//     default:
+//        console.log("sunday");
+//        break;
+
+    
+        
+//loops :
+// iteration statemets
+
+//loops-for,while,do while:
+
+//3 values
+// initialization
+//condition
+// increament/decrement
+
+//var a;
+// declaring a variable
+// var a=2;
+// we are initilizing the variable with the value of 2
+
+
+// for( let i=0 ;i<=5;i++)
+// {
+//     console.log(i);
+// }
+     for(let i=1;i<10;i++)
+     {
+         console.log("19*"+i "i");
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
